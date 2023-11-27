86bffb56a8e8ff8fa5ebfe564373e596cadbd1e2
ravcode9 <rlatypov2106@gmail.com>
2023-11-27 16:21:19 +0500

    added test_read_operations

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -13,3 +13,11 @@ def temp_json_file(tmp_path):
     with open(file_path, 'w', encoding='utf-8') as file:
         file.write(json_data)
     return file_path
+
+
+def test_read_operations_from_json(temp_json_file):
+    # Тестируем функцию read_operations_from_json
+    result = read_operations_from_json(temp_json_file)
+    assert result == [
+        {'date': '2023-11-27T12:34:56.789', 'description': 'Тестовая операция', 'from': '1234567890123456',
+         'to': '9876543210123456', 'operationAmount': {'amount': '100', 'currency': {'name': 'USD'}}}]
