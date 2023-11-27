6e33cfbac0d2ddea6aa2a1bc5ab5f519684df8fc
ravcode9 <rlatypov2106@gmail.com>
2023-11-27 16:23:06 +0500

    added test_read_operations

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -21,3 +21,12 @@ def test_read_operations_from_json(temp_json_file):
     assert result == [
         {'date': '2023-11-27T12:34:56.789', 'description': 'Тестовая операция', 'from': '1234567890123456',
          'to': '9876543210123456', 'operationAmount': {'amount': '100', 'currency': {'name': 'USD'}}}]
+
+
+def test_format_operation():
+    # Тестируем функцию format_operation на примере операции
+    operation = {'date': '2023-11-27T12:34:56.789', 'description': 'Тестовая операция', 'from': '1234567890123456',
+                 'to': '9876543210123456', 'operationAmount': {'amount': '100', 'currency': {'name': 'USD'}}}
+    expected_result = "27.11.2023 Тестовая операция\n1234 XX** **** 3456 -> **3456\n100 USD\n"
+    result = format_operation(operation)
+    assert result == expected_result
